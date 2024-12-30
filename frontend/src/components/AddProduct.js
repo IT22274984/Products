01b77f4076284fe83091db51a6aaf11c83994754
IT22274984 <127821714+IT22274984@users.noreply.github.com>
2024-12-30 13:59:10 +0530

    new

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import '../styles/AddProduct.css';
 
 export default function AddProduct() {
     let navigate = useNavigate();
@@ -46,27 +47,36 @@ export default function AddProduct() {
     return (
         <div className="container">
             <h2>Add New Product</h2>
-            <form onSubmit={addProduct}>
-                <div>
-                    <label>Product Number</label>
+            <form onSubmit={addProduct} className="my-form">
+                <div className="mb-3">
+                    <label htmlFor="productnumber" className="form-label">Product Number</label>
                     <input
                         type="text"
+                        className="form-control"
+                        id="productnumber"
+                        placeholder="Enter Product Number"
                         value={productnumber}
                         onChange={(e) => setProductNumber(e.target.value)}
                     />
                 </div>
-                <div>
-                    <label>Product Name</label>
+                <div className="mb-3">
+                    <label htmlFor="productname" className="form-label">Product Name</label>
                     <input
                         type="text"
+                        className="form-control"
+                        id="productname"
+                        placeholder="Enter Product Name"
                         value={productname}
                         onChange={(e) => setProductName(e.target.value)}
                     />
                 </div>
-                <div>
-                    <label>Dealer Price</label>
+                <div className="mb-3">
+                    <label htmlFor="productdealerprice" className="form-label">Dealer Price</label>
                     <input
                         type="number"
+                        className="form-control"
+                        id="productdealerprice"
+                        placeholder="Enter Dealer's Price"
                         value={productdealerprice}
                         onChange={(e) => {
                             setProductDealerPrice(e.target.value);
@@ -74,10 +84,13 @@ export default function AddProduct() {
                         }}
                     />
                 </div>
-                <div>
-                    <label>Profit Rate (%)</label>
+                <div className="mb-3">
+                    <label htmlFor="productprofitrate" className="form-label">Profit Rate (%)</label>
                     <input
                         type="number"
+                        className="form-control"
+                        id="productprofitrate"
+                        placeholder="Enter Profit Rate"
                         value={productprofitrate}
                         onChange={(e) => {
                             setProductProfitRate(e.target.value);
@@ -85,18 +98,24 @@ export default function AddProduct() {
                         }}
                     />
                 </div>
-                <div>
-                    <label>Selling Price</label>
-                    <input type="text" value={productsellingprice} readOnly />
+                <div className="mb-3">
+                    <label htmlFor="productsellingprice" className="form-label">Selling Price</label>
+                    <input type="text"
+                    className="form-control"
+                    id="productsellingprice" 
+                    value={productsellingprice} readOnly />
                 </div>
-                <div>
-                    <label>Collected Date</label>
+                <div className="mb-3">
+                    <label htmlFor="productcollecteddate" className="form-label">Collected Date</label>
                     <input
                         type="date"
+                        className="form-control"
+                        id="productcollecteddate"
                         value={productcollecteddate}
                         onChange={(e) => setProductCollectedDate(e.target.value)}
                     />
                 </div>
+                
                 <button type="submit">Add Product</button>
             </form>
         </div>
